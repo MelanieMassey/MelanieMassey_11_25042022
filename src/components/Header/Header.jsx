dcb664d4682fae30811fc6d32a5015a14b370878
Melanie Massey <nanie_d_enfer@hotmail.com>
2022-05-18 09:41:55 +0200

    Mise en forme text decoration des liens actifs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,36 @@
 import { Link } from "react-router-dom"
 import logo from "../../assets/logo.png"
 import "./Header.css"
+import { useState } from "react"
 
 function Header() {
+  const [homeCurrentPage, setHomeCurrentPage] = useState(true)
+  const [aboutCurrentPage, setAboutCurrentPage] = useState(false)
+
   return (
     <header>
       <img src={logo} className="logo" alt="logo Kasa" />
       <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A Propos</Link>
+        <Link
+          to="/"
+          className={homeCurrentPage ? "active" : ""}
+          onClick={() => {
+            setHomeCurrentPage(true)
+            setAboutCurrentPage(false)
+          }}
+        >
+          Accueil
+        </Link>
+        <Link
+          to="/about"
+          className={aboutCurrentPage ? "active" : ""}
+          onClick={() => {
+            setHomeCurrentPage(false)
+            setAboutCurrentPage(true)
+          }}
+        >
+          A Propos
+        </Link>
       </nav>
     </header>
   )
